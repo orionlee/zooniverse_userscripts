d6dac66adadfb79ef66ce119465295168ad521b2
orionlee <orionlee@yahoo.com>
2020-11-01 21:13:22 -0800

    SuperWASP - make flux to magnitude UI more mobile friendly

--- zn_superwasp_tweak.user.js
+++ zn_superwasp_tweak.user.js
@@ -7,7 +7,7 @@
 //                e.g., from Classify to Talk after users pressing Talk & Done.
 // @grant       GM_setClipboard
 // @grant       GM_addStyle
-// @version     1.11.1
+// @version     1.11.2
 // @author      -
 // @description UI 1) to help to follow up on a subject, looking up its information on SIMBAD, VSX, etc; and
 //                 2) make Classify UI more friendly on mobile / tablets (reducing scrolls needed).
@@ -123,7 +123,7 @@ function showSubjectFollowUpUI() {
     <a target="_simbad" href="http://simbad.u-strasbg.fr/simbad/sim-fcoo">SIMBAD</a><br>
     <a target="_nasa_superwasp" href="https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblSearch/nph-tblSearchInit?app=ExoTbls&config=superwasptimeseries">NASA exoplanet SuperWASP TS archive</a><br>
     <details>
-      <summary style="padding-top: 0.5em;">Flux to Magnitude</summary>
+      <summary style="padding-top: 0.5em; margin-bottom: 0.5em;">Flux to Magnitude</summary>
       Flux: <input id="subjectFollowUpInFlux" style="width: 10ch;"type="number">
       <button id="subjectFollowUpFluxToMagCtl" style="padding-left: 1.5ch;padding-right: 1.5ch;">Go</button>
       Mag.: <input id="subjectFollowUpOutMag" style="width: 10ch;" value="" tabindex="-1" readonly>
@@ -151,6 +151,18 @@ function showSubjectFollowUpUI() {
 
     document.getElementById('subjectFollowUpFluxToMagCtl2').onclick = () => {
       doFluxToMagnitudeWithUI('subjectFollowUpInFlux2', 'subjectFollowUpOutMag2');
+    };
+
+    document.getElementById('subjectFollowUpInFlux').onkeypress = (evt) => {
+      if (evt.code == 'Enter') {
+        doFluxToMagnitudeWithUI('subjectFollowUpInFlux', 'subjectFollowUpOutMag');
+      }
+    };
+
+    document.getElementById('subjectFollowUpInFlux2').onkeypress = (evt) => {
+      if (evt.code == 'Enter') {
+        doFluxToMagnitudeWithUI('subjectFollowUpInFlux2', 'subjectFollowUpOutMag2');
+      }
     };
 
     document.getElementById('subjectFollowUpCloseCtl').onclick = () => {
